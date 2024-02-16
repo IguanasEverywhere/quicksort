bbefed0ced18e1b6638ac32a2306c2fe4a567771
Scott Schwab <scottschwab86@gmail.com>
2024-02-16 12:52:02 -0600

    Complete challenge

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,9 +1,24 @@
 function partition(array, low, high) {
   // type your code in here
+  let pivotIdx = low;
+  for (let i = low + 1; i <= high; i++) {
+    if (array[i] < array[low]) {
+      [array[i], array[pivotIdx + 1]] = [array[pivotIdx + 1], array[i]];
+      pivotIdx++;
+    }
+  }
+  [array[low], array[pivotIdx]] = [array[pivotIdx], array[low]];
+  return pivotIdx;
 }
 
 function quicksort(array, low = 0, high = array.length - 1) {
   // type your code here
+  if (low < high) {
+    let pivotIdx = partition(array, low, high);
+    quicksort(array, low, pivotIdx - 1);
+    quicksort(array, pivotIdx + 1, high);
+  }
+  return array;
 }
 
 if (require.main === module) {
